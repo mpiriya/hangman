70f055dfd73daf5fdbdbe8398dee5b2243c4ac3c
mpiriya <piriya.matt@gmail.com>
2020-06-16 15:54:59 -0400

    refactored to class structure

--- hangman.rb
+++ hangman.rb
@@ -1,39 +1,51 @@
-def get_random_word 
-  words = Array.new
-
-  File.open("5desk.txt").readlines.each do |word|
-    words.push(word.chomp)
+class Hangman
+  def initialize
+    @rand_word = get_random_word
+    @guessed = []
+    @display = "_ "*rand_word.length
   end
 
-  rand_word = words[Random.rand(words.length)]
-end
+  def get_random_word 
+    words = Array.new
+  
+    File.open("5desk.txt").readlines.each do |word|
+      words.push(word.chomp)
+    end
+  
+    rand_word = words[Random.rand(words.length)]
+  end
+  
+  def make_guess(guess)
+    used = false
+    for i in 0..@rand_word.length-1
+      if @rand_word[i,1].downcase == guess.downcase
+        @display[i*2] = guess
+        used = true
+      end
+    end
+    if !used && @guessed.index(guess) == nil
+      @guessed.push(guess)
+    end 
+  end
 
-def make_guess(guess, rand_word, display, guessed)
-  used = false
-  for i in 0..rand_word.length-1
-    if rand_word[i,1].downcase == guess.downcase
-      display[i*2] = guess
-      used = true
+  def play
+    until @guessed.length == 6 || @display.index("_") == nil do
+      puts "\n\n\n\n\n#{@display}\nLetters guessed incorrectly: #{@guessed.join(" ")}\nGuesses remaining: #{6-@guessed.length}\nGuess a letter OR type 'save' to save your progress: "
+      guess = gets.chomp
+    
+      if guess.downcase == "save"
+        # save game
+      elsif guess.length == 1
+        make_guess(gets.chomp)
+      else
+        puts "\n\n\nInvalid guess length"
+      end
+    end
+    
+    if @guessed.length == 6
+      puts "YOU LOSE"
+    else
+      puts "#{@display}\n\nYOU WIN"
     end
   end
-  if !used && guessed.index(guess) == nil
-    guessed.push(guess)
-  end 
-end
-
-rand_word = get_random_word
-guessed = []
-display = "_ "*rand_word.length
-# puts rand_word
-
-until guessed.length == 6 || display.index("_") == nil do
-  puts "\n\n\n\n\n#{display}\nLetters guessed incorrectly: #{guessed.join(" ")}\nGuesses remaining: #{6-guessed.length}\nGuess a letter: "
-  
-  make_guess(gets.chomp, rand_word, display, guessed)
-end
-
-if guessed.length == 6
-  puts "YOU LOSE"
-else
-  puts "#{display}\n\nYOU WIN"
 end
